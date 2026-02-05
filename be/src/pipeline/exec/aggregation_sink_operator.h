zhengkaiyuan1993/doris

commit eb1c43ef751528cbbed28864ac6360331291a639

[fix](union) Fix union operator (#60334)

be/src/pipeline/exec/aggregation_sink_operator.h
@@ -131,14 +131,11 @@ protected:
 class AggSinkOperatorX MOCK_REMOVE(final) : public DataSinkOperatorX<AggSinkLocalState> {
 public:
     AggSinkOperatorX(ObjectPool* pool, int operator_id, int dest_id, const TPlanNode& tnode,
-                     const DescriptorTbl& descs, bool require_bucket_distribution);
+                     const DescriptorTbl& descs);
 
 #ifdef BE_TEST
     AggSinkOperatorX()
-            : DataSinkOperatorX<AggSinkLocalState>(1, 0, 2),
-              _is_first_phase(),
-              _is_colocate(),
-              _require_bucket_distribution() {}
+            : DataSinkOperatorX<AggSinkLocalState>(1, 0, 2), _is_first_phase(), _is_colocate() {}
 #endif
 
     ~AggSinkOperatorX() override = default;
@@ -148,6 +145,8 @@ public:
     }
 
     Status init(const TPlanNode& tnode, RuntimeState* state) override;
+    void update_operator(const TPlanNode& tnode, bool followed_by_shuffled_operator,
+                         bool require_bucket_distribution) override;
 
     Status prepare(RuntimeState* state) override;
 
@@ -160,11 +159,12 @@ public:
                            : DataSinkOperatorX<AggSinkLocalState>::required_data_distribution(
                                      state);
         }
-        return _is_colocate && _require_bucket_distribution && !_followed_by_shuffled_operator
+        return _is_colocate && _require_bucket_distribution
                        ? DataDistribution(ExchangeType::BUCKET_HASH_SHUFFLE, _partition_exprs)
                        : DataDistribution(ExchangeType::HASH_SHUFFLE, _partition_exprs);
     }
-    bool require_data_distribution() const override { return _is_colocate; }
+    bool is_colocated_operator() const override { return _is_colocate; }
+    bool is_shuffled_operator() const override { return !_partition_exprs.empty(); }
     size_t get_revocable_mem_size(RuntimeState* state) const;
 
     AggregatedDataVariants* get_agg_data(RuntimeState* state) {
@@ -221,9 +221,8 @@ protected:
     std::vector<int> _null_directions;
 
     bool _have_conjuncts;
-    const std::vector<TExpr> _partition_exprs;
+    std::vector<TExpr> _partition_exprs;
     const bool _is_colocate;
-    const bool _require_bucket_distribution;
     RowDescriptor _agg_fn_output_row_descriptor;
 };
 

be/src/pipeline/exec/aggregation_sink_operator.cpp
@@ -724,8 +724,7 @@ size_t AggSinkLocalState::get_reserve_mem_size(RuntimeState* state, bool eos) co
 
 // TODO: Tricky processing if `multi_distinct_` exists which will be re-planed by optimizer.
 AggSinkOperatorX::AggSinkOperatorX(ObjectPool* pool, int operator_id, int dest_id,
-                                   const TPlanNode& tnode, const DescriptorTbl& descs,
-                                   bool require_bucket_distribution)
+                                   const TPlanNode& tnode, const DescriptorTbl& descs)
         : DataSinkOperatorX<AggSinkLocalState>(operator_id, tnode, dest_id),
           _intermediate_tuple_id(tnode.agg_node.intermediate_tuple_id),
           _output_tuple_id(tnode.agg_node.output_tuple_id),
@@ -736,23 +735,26 @@ AggSinkOperatorX::AggSinkOperatorX(ObjectPool* pool, int operator_id, int dest_i
           _limit(tnode.limit),
           _have_conjuncts((tnode.__isset.vconjunct && !tnode.vconjunct.nodes.empty()) ||
                           (tnode.__isset.conjuncts && !tnode.conjuncts.empty())),
-          _partition_exprs(
-                  tnode.__isset.distribute_expr_lists &&
-                                  (require_bucket_distribution ||
-                                   std::any_of(
-                                           tnode.agg_node.aggregate_functions.begin(),
-                                           tnode.agg_node.aggregate_functions.end(),
-                                           [](const TExpr& texpr) -> bool {
-                                               return texpr.nodes[0]
-                                                       .fn.name.function_name.starts_with(
-                                                               vectorized::
-                                                                       DISTINCT_FUNCTION_PREFIX);
-                                           }))
-                          ? tnode.distribute_expr_lists[0]
-                          : tnode.agg_node.grouping_exprs),
           _is_colocate(tnode.agg_node.__isset.is_colocate && tnode.agg_node.is_colocate),
-          _require_bucket_distribution(require_bucket_distribution),
           _agg_fn_output_row_descriptor(descs, tnode.row_tuples) {}
+
+void AggSinkOperatorX::update_operator(const TPlanNode& tnode, bool followed_by_shuffled_operator,
+                                       bool require_bucket_distribution) {
+    _followed_by_shuffled_operator = followed_by_shuffled_operator;
+    _require_bucket_distribution = require_bucket_distribution;
+    _partition_exprs =
+            tnode.__isset.distribute_expr_lists &&
+                            (_followed_by_shuffled_operator ||
+                             std::any_of(
+                                     tnode.agg_node.aggregate_functions.begin(),
+                                     tnode.agg_node.aggregate_functions.end(),
+                                     [](const TExpr& texpr) -> bool {
+                                         return texpr.nodes[0].fn.name.function_name.starts_with(
+                                                 vectorized::DISTINCT_FUNCTION_PREFIX);
+                                     }))
+                    ? tnode.distribute_expr_lists[0]
+                    : tnode.agg_node.grouping_exprs;
+}
 
 Status AggSinkOperatorX::init(const TPlanNode& tnode, RuntimeState* state) {
     RETURN_IF_ERROR(DataSinkOperatorX<AggSinkLocalState>::init(tnode, state));

be/src/pipeline/exec/analytic_sink_operator.cpp
@@ -632,8 +632,7 @@ int64_t AnalyticSinkLocalState::find_first_not_equal(vectorized::IColumn* refere
 }
 
 AnalyticSinkOperatorX::AnalyticSinkOperatorX(ObjectPool* pool, int operator_id, int dest_id,
-                                             const TPlanNode& tnode, const DescriptorTbl& descs,
-                                             bool require_bucket_distribution)
+                                             const TPlanNode& tnode, const DescriptorTbl& descs)
         : DataSinkOperatorX(operator_id, tnode, dest_id),
           _pool(pool),
           _intermediate_tuple_id(tnode.analytic_node.intermediate_tuple_id),
@@ -642,10 +641,6 @@ AnalyticSinkOperatorX::AnalyticSinkOperatorX(ObjectPool* pool, int operator_id, 
                                      ? tnode.analytic_node.buffered_tuple_id
                                      : 0),
           _is_colocate(tnode.analytic_node.__isset.is_colocate && tnode.analytic_node.is_colocate),
-          _require_bucket_distribution(require_bucket_distribution),
-          _partition_exprs(tnode.__isset.distribute_expr_lists && require_bucket_distribution
-                                   ? tnode.distribute_expr_lists[0]
-                                   : tnode.analytic_node.partition_exprs),
           _window(tnode.analytic_node.window),
           _has_window(tnode.analytic_node.__isset.window),
           _has_range_window(tnode.analytic_node.window.type == TAnalyticWindowType::RANGE),

be/src/pipeline/exec/analytic_sink_operator.h
@@ -180,7 +180,7 @@ private:
 class AnalyticSinkOperatorX final : public DataSinkOperatorX<AnalyticSinkLocalState> {
 public:
     AnalyticSinkOperatorX(ObjectPool* pool, int operator_id, int dest_id, const TPlanNode& tnode,
-                          const DescriptorTbl& descs, bool require_bucket_distribution);
+                          const DescriptorTbl& descs);
 
 #ifdef BE_TEST
     AnalyticSinkOperatorX(ObjectPool* pool)
@@ -189,7 +189,6 @@ public:
               _output_tuple_id(0),
               _buffered_tuple_id(0),
               _is_colocate(false),
-              _require_bucket_distribution(false),
               _has_window(false),
               _has_range_window(false),
               _has_window_start(false),
@@ -202,6 +201,14 @@ public:
     }
 
     Status init(const TPlanNode& tnode, RuntimeState* state) override;
+    void update_operator(const TPlanNode& tnode, bool followed_by_shuffled_operator,
+                         bool require_bucket_distribution) override {
+        _followed_by_shuffled_operator = followed_by_shuffled_operator;
+        _require_bucket_distribution = require_bucket_distribution;
+        _partition_exprs = tnode.__isset.distribute_expr_lists && _followed_by_shuffled_operator
+                                   ? tnode.distribute_expr_lists[0]
+                                   : tnode.analytic_node.partition_exprs;
+    }
 
     Status prepare(RuntimeState* state) override;
 
@@ -210,13 +217,14 @@ public:
         if (_partition_by_eq_expr_ctxs.empty()) {
             return {ExchangeType::PASSTHROUGH};
         } else {
-            return _is_colocate && _require_bucket_distribution && !_followed_by_shuffled_operator
+            return _is_colocate && _require_bucket_distribution
                            ? DataDistribution(ExchangeType::BUCKET_HASH_SHUFFLE, _partition_exprs)
                            : DataDistribution(ExchangeType::HASH_SHUFFLE, _partition_exprs);
         }
     }
 
-    bool require_data_distribution() const override { return true; }
+    bool is_colocated_operator() const override { return _is_colocate; }
+    bool is_shuffled_operator() const override { return !_partition_by_eq_expr_ctxs.empty(); }
 
     size_t get_reserve_mem_size(RuntimeState* state, bool eos) override;
 
@@ -243,8 +251,7 @@ private:
     const TTupleId _buffered_tuple_id;
 
     const bool _is_colocate;
-    const bool _require_bucket_distribution;
-    const std::vector<TExpr> _partition_exprs;
+    std::vector<TExpr> _partition_exprs;
 
     TAnalyticWindow _window;
     bool _has_window;

be/src/pipeline/exec/distinct_streaming_aggregation_operator.cpp
@@ -321,17 +321,12 @@ void DistinctStreamingAggLocalState::_emplace_into_hash_table_to_distinct(
 
 DistinctStreamingAggOperatorX::DistinctStreamingAggOperatorX(ObjectPool* pool, int operator_id,
                                                              const TPlanNode& tnode,
-                                                             const DescriptorTbl& descs,
-                                                             bool require_bucket_distribution)
+                                                             const DescriptorTbl& descs)
         : StatefulOperatorX<DistinctStreamingAggLocalState>(pool, tnode, operator_id, descs),
           _output_tuple_id(tnode.agg_node.output_tuple_id),
           _needs_finalize(tnode.agg_node.need_finalize),
           _is_first_phase(tnode.agg_node.__isset.is_first_phase && tnode.agg_node.is_first_phase),
-          _partition_exprs(tnode.__isset.distribute_expr_lists && require_bucket_distribution
-                                   ? tnode.distribute_expr_lists[0]
-                                   : tnode.agg_node.grouping_exprs),
-          _is_colocate(tnode.agg_node.__isset.is_colocate && tnode.agg_node.is_colocate),
-          _require_bucket_distribution(require_bucket_distribution) {
+          _is_colocate(tnode.agg_node.__isset.is_colocate && tnode.agg_node.is_colocate) {
     if (tnode.agg_node.__isset.use_streaming_preaggregation) {
         _is_streaming_preagg = tnode.agg_node.use_streaming_preaggregation;
         if (_is_streaming_preagg) {

be/src/pipeline/exec/distinct_streaming_aggregation_operator.h
@@ -93,17 +93,24 @@ class DistinctStreamingAggOperatorX final
         : public StatefulOperatorX<DistinctStreamingAggLocalState> {
 public:
     DistinctStreamingAggOperatorX(ObjectPool* pool, int operator_id, const TPlanNode& tnode,
-                                  const DescriptorTbl& descs, bool require_bucket_distribution);
+                                  const DescriptorTbl& descs);
 #ifdef BE_TEST
     DistinctStreamingAggOperatorX()
             : _needs_finalize(false),
               _is_first_phase(true),
               _partition_exprs({}),
-              _is_colocate(false),
-              _require_bucket_distribution {false} {}
+              _is_colocate(false) {}
 #endif
 
     Status init(const TPlanNode& tnode, RuntimeState* state) override;
+    void update_operator(const TPlanNode& tnode, bool followed_by_shuffled_operator,
+                         bool require_bucket_distribution) override {
+        _followed_by_shuffled_operator = followed_by_shuffled_operator;
+        _require_bucket_distribution = require_bucket_distribution;
+        _partition_exprs = tnode.__isset.distribute_expr_lists && _followed_by_shuffled_operator
+                                   ? tnode.distribute_expr_lists[0]
+                                   : tnode.agg_node.grouping_exprs;
+    }
     Status prepare(RuntimeState* state) override;
     Status pull(RuntimeState* state, vectorized::Block* block, bool* eos) const override;
     Status push(RuntimeState* state, vectorized::Block* input_block, bool eos) const override;
@@ -114,14 +121,14 @@ public:
             return {ExchangeType::NOOP};
         }
         if (_needs_finalize || (!_probe_expr_ctxs.empty() && !_is_streaming_preagg)) {
-            return _is_colocate && _require_bucket_distribution && !_followed_by_shuffled_operator
+            return _is_colocate && _require_bucket_distribution
                            ? DataDistribution(ExchangeType::BUCKET_HASH_SHUFFLE, _partition_exprs)
                            : DataDistribution(ExchangeType::HASH_SHUFFLE, _partition_exprs);
         }
         return {ExchangeType::PASSTHROUGH};
     }
 
-    bool require_data_distribution() const override { return _is_colocate; }
+    bool is_colocated_operator() const override { return _is_colocate; }
 
 private:
     friend class DistinctStreamingAggLocalState;
@@ -130,9 +137,8 @@ private:
     TupleDescriptor* _output_tuple_desc = nullptr;
     const bool _needs_finalize;
     const bool _is_first_phase;
-    const std::vector<TExpr> _partition_exprs;
+    std::vector<TExpr> _partition_exprs;
     const bool _is_colocate;
-    const bool _require_bucket_distribution;
     // group by k1,k2
     vectorized::VExprContextSPtrs _probe_expr_ctxs;
     std::vector<size_t> _make_nullable_keys;

be/src/pipeline/exec/hashjoin_build_sink.h
@@ -140,11 +140,13 @@ public:
     }
 
     bool is_shuffled_operator() const override {
-        return _join_distribution == TJoinDistributionType::PARTITIONED;
+        return _join_distribution == TJoinDistributionType::PARTITIONED ||
+               _join_distribution == TJoinDistributionType::BUCKET_SHUFFLE ||
+               _join_distribution == TJoinDistributionType::COLOCATE;
     }
-    bool require_data_distribution() const override {
-        return _join_distribution != TJoinDistributionType::BROADCAST &&
-               _join_distribution != TJoinDistributionType::NONE;
+    bool is_colocated_operator() const override {
+        return _join_distribution == TJoinDistributionType::BUCKET_SHUFFLE ||
+               _join_distribution == TJoinDistributionType::COLOCATE;
     }
     std::vector<bool>& is_null_safe_eq_join() { return _is_null_safe_eq_join; }
 
@@ -161,7 +163,7 @@ private:
     std::vector<bool> _is_null_safe_eq_join;
 
     bool _is_broadcast_join = false;
-    const std::vector<TExpr> _partition_exprs;
+    std::vector<TExpr> _partition_exprs;
 
     std::vector<SlotId> _hash_output_slot_ids;
     std::vector<bool> _should_keep_column_flags;

be/src/pipeline/exec/hashjoin_probe_operator.h
@@ -147,11 +147,13 @@ public:
     bool is_broadcast_join() const { return _is_broadcast_join; }
 
     bool is_shuffled_operator() const override {
-        return _join_distribution == TJoinDistributionType::PARTITIONED;
+        return _join_distribution == TJoinDistributionType::PARTITIONED ||
+               _join_distribution == TJoinDistributionType::BUCKET_SHUFFLE ||
+               _join_distribution == TJoinDistributionType::COLOCATE;
     }
-    bool require_data_distribution() const override {
-        return _join_distribution != TJoinDistributionType::BROADCAST &&
-               _join_distribution != TJoinDistributionType::NONE;
+    bool is_colocated_operator() const override {
+        return _join_distribution == TJoinDistributionType::BUCKET_SHUFFLE ||
+               _join_distribution == TJoinDistributionType::COLOCATE;
     }
 
     bool need_finalize_variant_column() const { return _need_finalize_variant_column; }
@@ -192,7 +194,7 @@ private:
     bool _need_finalize_variant_column = false;
     std::set<int> _should_not_lazy_materialized_column_ids;
     std::vector<std::string> _right_table_column_names;
-    const std::vector<TExpr> _partition_exprs;
+    std::vector<TExpr> _partition_exprs;
 
     // Index of column(slot) from right table in the `_intermediate_row_desc`.
     size_t _right_col_idx;

commit 705ca19d56c9c406f7334b88c99b9f24d1471ae6

[fix](agg) Fix local shuffle before agg (#60494)
This bug was introduced by #60334

be/src/pipeline/exec/aggregation_sink_operator.h
@@ -164,7 +164,9 @@ public:
                        : DataDistribution(ExchangeType::HASH_SHUFFLE, _partition_exprs);
     }
     bool is_colocated_operator() const override { return _is_colocate; }
-    bool is_shuffled_operator() const override { return !_partition_exprs.empty(); }
+    bool is_shuffled_operator() const override {
+        return !_partition_exprs.empty() && _needs_finalize;
+    }
     size_t get_revocable_mem_size(RuntimeState* state) const;
 
     AggregatedDataVariants* get_agg_data(RuntimeState* state) {